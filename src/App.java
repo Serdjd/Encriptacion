57efdf97f66d9309bc69194480c39be0be23fe9e
Serdjd <segiodobladomunoz@gmail.com>
2023-05-16 10:15:18 +0200

    se añade encriptador y se modifica App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.File;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   public static void main(String[] args) throws Exception {
+        File fichero = new File("texto.txt");
+        String texto;
+        if(!fichero.exists()){
+            System.out.println("El archivo no existe");
+            System.exit(0);
+        }
+        FileReader read = new FileReader(fichero);
+        FileWriter writer = new FileWriter(fichero,true);
+        Scanner leer = new Scanner(System.in);
+        Scanner leer_fichero = new Scanner(fichero);
+        System.out.println("Imprime el texto por pantalla");
+        texto=leer.nextLine();
+        
     }
 }
---
+++ src/Encriptador.java
@@ -0,0 +1,12 @@
+public class Encriptador {
+    public StringBuilder encripter(String text,int mecanismo){
+        StringBuilder nuevotxt = new StringBuilder();
+        for(int i=0;i<text.length();i++){
+            char temp =(char) (text.charAt(i)+2);
+            if(){
+                
+            }
+            nuevotxt.append(temp);
+        }
+    }
+}
